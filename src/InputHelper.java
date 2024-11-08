4a711a2e8df892def97d2667c6c6ebef3dd8a9a9
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-08 08:22:45 -0500

    2ini

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class InputHelper {
+    public static double getRangedDouble(Scanner scan, String prompt, double min, double max) {
+        double num = 0;
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                num = scan.nextDouble();
+                if (num >= min && num <= max) {
+                    check = true;
+                } else {
+                    System.out.println("Input not within the range, please try again.");
+                }
+            } else {
+                System.out.println("Invalid input, please try again.");
+            }
+            scan.nextLine();
+        } while (!check);
+
+        return num;
+    }
+
+    public static boolean getYNConfirm (Scanner scan, String prompt) {
+        String input;
+        boolean confirm = false;
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            input = scan.nextLine();
+            if (input.equalsIgnoreCase("y")) {
+                confirm = true;
+                check = true;
+            } else if (input.equalsIgnoreCase("n")) {
+                check = true;
+            } else {
+                System.out.println("Invalid input, please try again.");
+            }
+        } while (!check);
+
+        return confirm;
+    }
 }
--- src/TaskFourCheckOut.java
+++ src/TaskFourCheckOut.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TaskFourCheckOut {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //variable
+        boolean contin;
+        double total = 0;
+        int item = 0;
+
+        do {
+            item = item + 1;
+            double price = InputHelper.getRangedDouble(scan, "What is the price of your item? [$0.50-$9.99]", 0.05, 9.99);
+            total += price;
+            contin = InputHelper.getYNConfirm(scan, "Do you want to continue to add item? [Y/N]");
+        } while (contin);
+
+        System.out.printf("%-18s %5.2f", "Total cost in $:", total);
+    }
 }
